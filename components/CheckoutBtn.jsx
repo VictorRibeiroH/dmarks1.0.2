eb28292ea1db6832c95d234eca367e98ed98582d
Victor Ribeiro <contato.victoribeiro@gmail.com>
2024-02-19 19:03:59 -0300

    link whatsapp checkout

--- components/CheckoutBtn.jsx
+++ components/CheckoutBtn.jsx
@@ -1,8 +1,9 @@
+import Link from "next/link"
 
 
 const CheckoutBtn = () => {
   return (
-    <button className="btn btn-primary w-full">Solicite seu orçamento!</button>
+    <Link href="https://wa.me/5541984613554?text=Ol%C3%A1%2C+vim+pelo+site+e+gostaria+de+cota%C3%A7%C3%A3o+dos+produtos%3A+" target="_blank" className="btn btn-primary w-full">Solicite seu orçamento!</Link>
   )
 }
 
