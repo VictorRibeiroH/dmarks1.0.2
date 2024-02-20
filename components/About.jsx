76a88817b650e74c5b4e6c371e2e739afd740705
Victor Ribeiro <contato.victoribeiro@gmail.com>
2024-02-20 20:17:19 -0300

    about margin

--- components/About.jsx
+++ components/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="max-w-container mx-auto px-4">
+    <div className="max-w-container mx-auto px-4 mt-10">
       <div className="flex pb-10">
         <div className="w-[60%] pb-10">
           <h1 className="max-w-[600px] text-base text-lightText mb-2">
@@ -10,18 +10,15 @@ const About = () => {
               <span className="text-black text-4xl">DMARKS LOCAÇÕES</span>
             </span>{" "}
             <span className="text-gray-500">
-              <br />
-              <br />
+              <br /><br />
               Fundada em 2002, na cidade de Curitiba/PR, a D'marks Locações é uma empresa
               especializada em locação de móveis para feiras e eventos em geral, sendo uma das
               pioneiras no ramo.
-              <br />
-              <br />
+              <br /><br />
               A empresa trabalha com profissionais treinados, visando sempre a qualidade dos produtos
               e serviços prestados. Nossa principal missão é atender e satisfazer as necessidades de nossos clientes,
-              buscando melhorar constantemente o atendimento e o serviço prestado, com profissionais capacitados e materiais de primera linha
-              <br />
-              <br />
+              buscando melhorar constantemente o atendimento e o serviço prestado, com profissionais capacitados e materiais de primeira linha
+              <br /><br />
               Para manter esse compromisso, a D'marks Locações, além de toda a estrutura
               própria, possui um estoque amplo e variado e está sempre inovando com qualidade e bom
               gosto, acompanhando as tendências do mercado, proporcionando aos seus clientes e 
@@ -33,10 +30,10 @@ const About = () => {
           <iframe
             width="100%"
             height="400"
-            frameborder="0"
+            frameBorder="0"
             style={{ border: "0" }}
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14419.196956669206!2d-49.3862087!3d-25.3780441!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dce032fa004613%3A0xc909e5c4c42e08a1!2sDmarks%20-%20Loca%C3%A7%C3%A3o%20de%20M%C3%B3veis%20em%20Curitiba!5e0!3m2!1spt-BR!2sbr!4v1708469410159!5m2!1spt-BR!2sbr"
-            allowfullscreen
+            allowFullScreen
           ></iframe>
         </div>
       </div>
