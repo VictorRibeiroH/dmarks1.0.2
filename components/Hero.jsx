dc5c34929c47199d91a9df0083f3f1ef39751053
Victor Ribeiro <contato.victoribeiro@gmail.com>
2024-02-20 18:01:40 -0300

    tests hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 const Hero = () => {
   return (
-    <section className="h-screen relative overflow-hidden bg-primary/5">
-      <div className="container mx-auto h-full relative">
-        <div className="flex items-center justify-between h-full">
+    <section className="h-screen relative overflow-hidden bg-grey flex justify-center items-center">
+      <div className="container mx-auto">
+        <div className="flex items-center justify-center h-full">
           {/* Imagem */}
-          <div className="hidden xl:flex h-full absolute inset-0 z-0 opacity-50">
+          <div className="hidden xl:flex h-[650px] w-full absolute inset-0 z-0 opacity-50">
             <Image
-              src="/bg/hero2.png"
+              src="/bg/bg-hero.png"
               layout="fill"
               objectFit="cover"
               alt="logo dmarks"
@@ -17,10 +17,12 @@ const Hero = () => {
             />
           </div>
           {/* Texto */}
-          <div className="w-full xl:max-w-[500px] flex flex-col justify-center items-start absolute z-10">
-            <h1>
+          <div className="w-full xl:max-w-[500px] flex flex-col justify-center items-center absolute z-10">
+            <h1 className="text-4xl md:text-5xl lg:text-6xl text-center">
               Locação de Móveis    
             </h1> 
+            <h2>Para feiras e eventos</h2> 
+            <span className="text-lg text-center font-semibold">A satisfação do seu cliente é o melhor projeto para a sua empresa</span>
           </div>
         </div>
       </div>
