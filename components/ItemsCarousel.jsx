736a6ab094961db6a6a68d63634574ea07b484ad
Victor Ribeiro <contato.victoribeiro@gmail.com>
2024-02-23 04:12:37 -0300

    não consegui a seta. kk

--- components/ItemsCarousel.jsx
+++ components/ItemsCarousel.jsx
@@ -1,37 +1,34 @@
-"use client";
+'use client';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
+import {Swiper, SwiperSlide} from 'swiper/react'
+
+import 'swiper/css'
 import 'swiper/css/pagination';
-import SwiperCore, { Navigation, Pagination } from 'swiper';
 
+import {Pagination} from 'swiper/modules'
 import Item from './Item';
 
-SwiperCore.use([Navigation, Pagination]);
-
-const ItemsCarousel = ({ bikes }) => {
+const ItemsCarousel = ({bikes}) => {
   return (
-    <Swiper
-      slidesPerView={1}
-      spaceBetween={30}
-      breakpoints={{
-        640: { slidesPerView: 1 },
-        768: { slidesPerView: 2 },
-        960: { slidesPerView: 3 },
-        1440: { slidesPerView: 4 },
-      }}
-      navigation={true}
-      className='popular-bike-slider mb-8'
+    <Swiper slidesPerView={1} spaceBetween={30} breakpoints={{
+      640: { slidesPerView: 1 },
+      768: { slidesPerView: 2 },
+      960: { slidesPerView: 3 },
+      1440: { slidesPerView: 4 },
+    }}
+    pagination={{
+      clickable: true,
+    }}
+    modules={[Pagination]}
+    className='popular-bike-slider mb-8'
     >
-      {bikes.map((bike) => (
-        <SwiperSlide key={bike._id}>
-          <Item bike={bike}/>
-        </SwiperSlide>
-      ))}
+        {bikes.map((bike) => {
+            return <SwiperSlide key={bike._id}>
+                <Item bike={bike}/>
+            </SwiperSlide>
+        })}
     </Swiper>
-  );
+  )
 }
 
-export default ItemsCarousel;
-
+export default ItemsCarousel
