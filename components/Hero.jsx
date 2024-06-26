7e3b8d33105ba878b7a252b77cc85969f3df3bd2
charlesalmeiida <charlesalmeida844@gmail.com>
2024-06-25 22:55:01 -0300

    fix: hero's background

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
-import SearchBar from "@/components/SearchBar"; // Importe o componente de SearchBar
-import About from "./About";
-import BtnProd from "./BtnProd";
+import Image from "next/image"
+import Link from "next/link"
+import SearchBar from "@/components/SearchBar" // Importe o componente de SearchBar
+import About from "./About"
+import BtnProd from "./BtnProd"
 
 const Hero = () => {
   return (
@@ -10,7 +10,7 @@ const Hero = () => {
       <div className="container mx-auto">
         <div className="flex items-center justify-center h-full">
           {/* Imagem */}
-          <div className="hidden xl:flex h-[88%] w-full absolute inset-0 z-0 opacity-50">
+          <div className=" xl:flex h-[88%] w-full absolute inset-0 z-0 opacity-50">
             <Image
               src="/bg/bg-cwb.jpg"
               layout="fill"
@@ -23,25 +23,36 @@ const Hero = () => {
           {/* Texto */}
           <div className="w-full xl:max-w-[500px] flex flex-col justify-center items-center absolute z-10">
             <h1 className="text-4xl md:text-5xl lg:text-6xl text-center">
-              Locação de Móveis    
-            </h1> 
-            <h2 className="text-4xl">Para feiras e eventos</h2> 
-            <span className="text-xl text-center font-semibold mt-4 text-gray-800 italic whitespace-nowrap">A satisfação do seu cliente é o melhor projeto para a sua empresa</span>
+              Locação de Móveis
+            </h1>
+            <h2 className="text-4xl">Para feiras e eventos</h2>
+            <span className="text-xl px-4 text-center font-semibold mt-4 text-gray-800 italic md:whitespace-wrap">
+              A satisfação do seu cliente é o melhor projeto para a sua empresa
+            </span>
             {/* SearchBar */}
             {/* <SearchBar /> */}
             {/* Botão Produtos */}
-            <BtnProd /> 
+            <BtnProd />
           </div>
         </div>
         {/* Ícone de seta para baixo */}
         <div className="flex justify-center mt-40">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-gray-800 animate-bounce" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M10 2a1 1 0 0 1 1 1v11.586l2.293-2.293a1 1 0 1 1 1.414 1.414l-4 4a1 1 0 0 1-1.414 0l-4-4a1 1 0 1 1 1.414-1.414L9 14.586V3a1 1 0 0 1 1-1z" clipRule="evenodd" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8 text-gray-800 animate-bounce"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 2a1 1 0 0 1 1 1v11.586l2.293-2.293a1 1 0 1 1 1.414 1.414l-4 4a1 1 0 0 1-1.414 0l-4-4a1 1 0 1 1 1.414-1.414L9 14.586V3a1 1 0 0 1 1-1z"
+              clipRule="evenodd"
+            />
           </svg>
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
