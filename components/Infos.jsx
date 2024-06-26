c4afa38d5b2ae20326af3f84d58be8fbb374f75a
charlesalmeiida <charlesalmeida844@gmail.com>
2024-06-25 23:06:36 -0300

    fix: adjust the flex-direction on infos component

--- components/Infos.jsx
+++ components/Infos.jsx
@@ -1,25 +1,46 @@
-"use client";
+"use client"
 
-import React from 'react';
+import React from "react"
 
 const Slider = () => {
   const slides = [
-    { title: 'Missão', description: 'Fornecer móveis e produtos de decoração modernos e de qualidade para o seu evento com preços competitivos.' },
-    { title: 'Visão', description: 'Ser empresa de referência no fornecimento de móveis para eventos, reconhecida pela qualidade de seus produtos, agilidade na entrega e no atendimento.' },
-    { title: 'Valores', description: 'Qualidade, agilidade e confiança.' }
-  ];
+    {
+      title: "Missão",
+      description:
+        "Fornecer móveis e produtos de decoração modernos e de qualidade para o seu evento com preços competitivos.",
+    },
+    {
+      title: "Visão",
+      description:
+        "Ser empresa de referência no fornecimento de móveis para eventos, reconhecida pela qualidade de seus produtos, agilidade na entrega e no atendimento.",
+    },
+    { title: "Valores", description: "Qualidade, agilidade e confiança." },
+  ]
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '77px', marginLeft: '60px' }}>
+    <div
+      className="flex-col md:flex-row gap-10"
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        marginTop: "77px",
+      }}
+    >
       {slides.map((slide, index) => (
-        <div key={index} style={{ flex: 1, textAlign: 'center', padding: '0 20px' }}>
-          <h1 style={{ fontSize: '2em', fontWeight: 'bold', margin: '0' }}>{slide.title}</h1>
-          <p style={{ fontSize: '1.3em', maxWidth: '600px', margin: '0 auto' }}>{slide.description}</p>
+        <div
+          key={index}
+          style={{ flex: 1, textAlign: "center", padding: "0 20px" }}
+        >
+          <h1 style={{ fontSize: "2em", fontWeight: "bold", margin: "0" }}>
+            {slide.title}
+          </h1>
+          <p style={{ fontSize: "1.3em", maxWidth: "600px", margin: "0 auto" }}>
+            {slide.description}
+          </p>
         </div>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default Slider;
-
+export default Slider
