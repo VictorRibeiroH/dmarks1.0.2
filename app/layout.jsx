c406d8d44ecdcebc8bbe18069a7efc75748b2254
charlesalmeiida <charlesalmeida844@gmail.com>
2024-07-04 17:27:28 -0300

    fix: thumb image

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,41 +16,21 @@ export const metadata = {
   title: "DMarks | Locação de móveis",
   description:
     "A D'Marks Locações, fundada em 2002 em Curitiba/PR, é especializada na locação de móveis para feiras e eventos. Com profissionais capacitados e materiais de alta qualidade, nossa missão é satisfazer as necessidades dos clientes, garantindo atendimento de excelência e inovação constante.",
-  url: "https://dmarkslocacoes.com.br",
-  image: "https://dmarkslocacoes.com.br/thumb.png", // URL completa da imagem
+  openGraph: {
+    images: "https://dmarkslocacoes.com.br/thumb.png",
+  },
+  twitter: {
+    images: "https://dmarkslocacoes.com.br/thumb.png",
+  },
+  keywords: ["Móveis", "Eventos", "Feira", "Mobília", "Locação"],
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{metadata.title}</title>
-        <meta
-          name="keywords"
-          content="movéis, locação, eventos, imobiliário, feiras"
-        />
         <meta name="description" content={metadata.description} />
-        {/* Open Graph meta tags */}
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.image} />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:url" content={metadata.url} />
-        <meta property="og:type" content="website" />
-        {/* Twitter Card meta tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={metadata.title} />
-        <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content={metadata.image} />
-        <link
-          rel="icon"
-          href="/favicon.ico"
-          type="image/x-icon"
-          sizes="16x16"
-        />
       </Head>
       <body className={rajdhani.variable}>
         <CartProvider>
--- app/page.jsx
+++ app/page.jsx
@@ -1,39 +1,11 @@
-import Head from "next/head"
 import Hero from "@/components/Hero"
 import Popular from "@/components/Popular"
 
-const metadata = {
-  title: "DMarks | Locação de móveis",
-  description:
-    "A D'Marks Locações, fundada em 2002 em Curitiba/PR, é especializada na locação de móveis para feiras e eventos. Com profissionais capacitados e materiais de alta qualidade, nossa missão é satisfazer as necessidades dos clientes, garantindo atendimento de excelência e inovação constante.",
-  url: "https://dmarkslocacoes.com.br",
-  image: "https://dmarkslocacoes.com.br/thumb.png", // URL completa da imagem
-}
-
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        {/* Open Graph meta tags */}
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.image} />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:url" content={metadata.url} />
-        <meta property="og:type" content="website" />
-        {/* Twitter Card meta tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={metadata.title} />
-        <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content={metadata.image} />
-      </Head>
-      <main>
-        <Hero />
-        <Popular />
-      </main>
-    </>
+    <main>
+      <Hero />
+      <Popular />
+    </main>
   )
 }
